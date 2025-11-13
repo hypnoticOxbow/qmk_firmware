2210c95e2c36c57d3442f2ee6a4b6180dde69910
Ian Kariniemi <i.kariniemi@gmail.com>
2025-11-12 16:56:44 -0800

    lappy windows key is borken.

--- keyboards/redox/keymaps/hoodunit/keymap.c
+++ keyboards/redox/keymaps/hoodunit/keymap.c
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,TAP_SYM ,TAP_RSYM ,        KC_SCLN ,TAP_RSYM ,KC_N   ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,KC_LALT ,KC_RGUI ,TAP_NAV,     KC_LALT    ,KC_SPC   ,TAP_RQWER,        KC_SPC  ,TAP_RQWER,   KC_RGUI ,     TAP_NAV ,KC_ALGR ,KC_BRID ,KC_BRIU
+     XXXXXXX ,KC_LALT ,KC_RGUI ,TAP_NAV,     KC_LALT    ,KC_SPC   ,TAP_RQWER,        KC_SPC  ,TAP_RQWER,   KC_LALT ,     TAP_NAV ,KC_ALGR ,KC_BRID ,KC_BRIU
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH , KC_BSLS, XXXXXXX,        XXXXXXX ,TAP_RSYM ,  KC_Z  ,  KC_X  ,  KC_C   , KC_V  , KC_B   , KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,KC_RGUI ,KC_SCLN,      KC_LALT ,    KC_SPC ,TAP_RQWER ,        KC_SPC ,TAP_RQWER  , KC_RGUI ,     KC_SCLN ,KC_ALGR ,KC_BRID ,KC_BRIU
+     XXXXXXX ,XXXXXXX ,KC_RGUI ,KC_SCLN,      KC_LALT ,    KC_SPC ,TAP_RQWER ,        KC_SPC ,TAP_RQWER  , KC_LALT ,     KC_SCLN ,KC_ALGR ,KC_BRID ,KC_BRIU
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
